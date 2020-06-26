1d945a7ee41327d4ea2f18b345a494a963ae3030
Alemaskina Alina <39837697+asalemaskina@users.noreply.github.com>
2020-06-26 22:52:00 +0300

    Add files via upload

--- server/main.cpp
+++ server/main.cpp
@@ -1,4 +1,4 @@
-#include "telnet.h"
+#include "telnet.hpp"
 #define MAX_LINE 16384
 
 std::vector<std::string> strVec(60);
@@ -15,15 +15,16 @@ void readcb(struct bufferevent *bev, void *ctx)
     std::string dir = *dir1;
     int fd=std::atoi(dir.c_str());
 
-    line = std::string(evbuffer_readln(input, &n, EVBUFFER_EOL_LF));
+    line = evbuffer_readln(input, &n, EVBUFFER_EOL_LF);
+   // line.erase(line.length()-1,2);
     if ((line[0]=='c')&&(line[1]=='d')){
         line.erase(0,3);
         ::strVec[fd]=line;
         line="";
     }
     line=cmd(line, ::strVec[fd]);
-        evbuffer_add(output, line.c_str(), line.length());
-        evbuffer_add(output, "\n", 1);
+    evbuffer_add(output, line.c_str(), line.length());
+    evbuffer_add(output, "\n", 1);
 
     if (evbuffer_get_length(input) >= MAX_LINE) {
 
@@ -31,7 +32,7 @@ void readcb(struct bufferevent *bev, void *ctx)
             int n = evbuffer_remove(input, (void *)line.c_str(), line.length());
 
             line=cmd(line,dir);
-                evbuffer_add(output, line.c_str(), line.length());
+            evbuffer_add(output, line.c_str(), line.length());
         }
         evbuffer_add(output, "\n", 1);
     }
@@ -42,7 +43,7 @@ void errorcb(struct bufferevent *bev, short error, void *ctx)
     if (error & BEV_EVENT_EOF) {
         perror("connection closed");
     } else if (error & BEV_EVENT_ERROR) {
-       perror("event error");
+        perror("event error");
     }
 
     bufferevent_free(bev);
@@ -61,6 +62,7 @@ void do_accept(evutil_socket_t listener, short event, void *arg)
         ::strVec[fd]="/root";
         struct bufferevent *bev;
         evutil_make_socket_nonblocking(fd);
+        std::cout<<fd;
         bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
         bufferevent_setcb(bev, readcb, NULL, errorcb,  static_cast<void*>(new std::string(std::to_string(fd))));
         bufferevent_setwatermark(bev, EV_READ, 0, MAX_LINE);
@@ -74,8 +76,8 @@ void run(void)
     Socket sock;
     sock.create();
     sock.setNonBlock();
-    sock.bind(23000);
-    sock.listen(16);
+    sock.bind(3002);
+    sock.listen(160);
     Event ev(base.getEventBase(), sock.getFd(), EV_READ|EV_PERSIST, do_accept);
     ev.add(NULL);
     base.dispatch();
---
+++ server/telnet.hpp
@@ -0,0 +1,213 @@
+#include<unistd.h>
+#include<fcntl.h>
+
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+
+#include<stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <iostream>
+#include <event2/event-config.h>
+#include <event2/event.h>
+#include <event2/event_struct.h>
+#include <event2/util.h>
+#include <event2/listener.h>
+#include <event2/buffer.h>
+#include <event2/bufferevent.h>
+#include <vector>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+std::string cmd(std::string arg, std::string dir)
+{
+    std::string data;
+    FILE * stream;
+    const int max_buffer = 256;
+    char buffer[max_buffer];
+    chdir(dir.c_str());
+    if (arg.length()){
+        stream = popen(arg.c_str(), "r");
+        if (stream) {
+            while (!feof(stream))
+                if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
+            pclose(stream);
+        }
+    }
+    return data;
+}
+
+class Socket{
+public:
+    Socket():fd_(-1){
+    }
+    explicit Socket(int fd):fd_(fd){
+    }
+
+    void create(){
+        fd_ = ::socket(AF_INET, SOCK_STREAM, 0);
+    }
+
+    void bind(short port){
+        struct sockaddr_in addr;
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr =0;
+        addr.sin_port = ::htons(port);
+
+        if(::bind(fd_,(struct sockaddr*)&addr, sizeof(addr))<0)
+        {
+            std::cout<<"bind :(\n" << strerror(errno);
+            exit(0);
+        }
+    }
+
+    void listen(int backlog = 2){
+        if(::listen(fd_, backlog)<0)
+        {
+            std::cout<<"listen:(\n";
+            exit(0);
+        }
+    }
+
+
+
+    void close(){
+        ::close(fd_);
+    }
+
+    int getFd() const {
+        return fd_;
+    }
+
+
+
+    void setNonBlock(){
+        int flag;
+        if ((flag = ::fcntl(fd_, F_GETFL)) == -1){
+            std::cout<<"bad\n";
+        }
+        flag |= O_NONBLOCK;
+        if (::fcntl(fd_, F_SETFL, flag) == -1){
+            std::cout<<"bad\n";
+        }
+    }
+
+
+private:
+    Socket(const Socket&);
+    Socket& operator=(const Socket&);
+private:
+    int fd_;
+};
+
+
+typedef void(*EventCallback)(int, short, void*);
+
+
+class EventBase{
+public:
+
+    EventBase(){
+        evbase_ = event_base_new();
+        if (evbase_ == NULL){
+            std::cout<<"bad\n";
+        }
+    }
+
+    ~EventBase(){
+        event_base_free(evbase_);
+    }
+    inline operator struct event_base*()
+    {
+        return evbase_;
+    }
+
+    inline struct event_base* base()
+    {
+        return evbase_;
+    }
+
+    void loop(int flags=0){
+        if (event_base_loop(evbase_, flags) != 0){
+            std::cout<<"bad\n";
+        }
+    }
+
+    void dispatch(){
+        if (event_base_dispatch(evbase_) != 0){
+            std::cout<<"bad in dispatch\n";
+        }
+    }
+
+
+
+    void loopBreak(){
+        if (event_base_loopbreak(evbase_) != 0){
+            std::cout<<"bad\n";
+        }
+    }
+
+    struct event_base* getEventBase() const{
+        return evbase_;
+    }
+
+private:
+    struct event_base* evbase_;
+};
+
+
+class Event{
+public:
+    Event(): ev_(NULL){
+
+    }
+    ~Event()
+    {
+        free();
+    }
+    Event(struct event_base* base, int fd,
+          short what, EventCallback cb){
+        ev_ = event_new(base, fd, what, cb, static_cast<void *>(base));
+        if (ev_ == NULL){
+            std::cout<<"bad in construct\n";
+        }
+    }
+
+
+    void free()
+    {
+        if (ev_)
+        {
+            event_free(ev_);
+        }
+        ev_ = NULL;
+    }
+    void add(const struct timeval* tv){
+        if (event_add(ev_, tv) != 0){
+            std::cout<<"bad in add\n";
+        }
+    }
+
+    struct event* getEvent(){
+        return ev_;
+    }
+protected:
+    Event(const EventBase& base, int fd,
+          short what, EventCallback cb, void* arg){
+        ev_ = event_new(base.getEventBase(), fd, what, cb, arg);
+        if (ev_ == NULL){
+            std::cout<<"bad\n";
+        }
+    }
+private:
+    struct event* ev_;
+};
+
+
